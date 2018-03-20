eb926ab42c6e8a177641084d27dff26d260c9bd1
Rob Doherty <robertdoherty@hotmail.co.uk>
2018-03-20 11:47:29 +0000

    refactored Jurisdiction to follow latest frontend service changes

--- src/test/scala/simulations/divorce/Jurisdiction.scala
+++ src/test/scala/simulations/divorce/Jurisdiction.scala
@@ -5,38 +5,38 @@ import io.gatling.http.Predef._
 
 import com.typesafe.config._
 
-object NewJurisdiction {
+object Jurisdiction {
 
     val conf = ConfigFactory.load()
     val baseurl: String = System.getenv("E2E_FRONTEND_URL")
     val continuePause = conf.getInt("continuePause")
 
 
-    val habitualResidence = exec(http("/njurisdiction/habitual-residence")
-        .post("/njurisdiction/habitual-residence")
+    val habitualResidence = exec(http("/jurisdiction/habitual-residence")
+        .post("/jurisdiction/habitual-residence")
         .formParam("jurisdictionPetitionerResidence", "No")
         .formParam("jurisdictionRespondentResidence", "No")
         .check(status.is(200))
-        .check(currentLocation.is(baseurl + "/njurisdiction/domicile")))
+        .check(currentLocation.is(baseurl + "/jurisdiction/domicile")))
         .pause(continuePause)
 
-    val domicile = exec(http("/njurisdiction/domicile")
-        .post("/njurisdiction/domicile")
+    val domicile = exec(http("/jurisdiction/domicile")
+        .post("/jurisdiction/domicile")
         .formParam("jurisdictionPetitionerDomicile", "No")
         .formParam("jurisdictionRespondentDomicile", "No")
         .check(status.is(200))
-        .check(currentLocation.is(baseurl + "/njurisdiction/last-habitual-residence")))
+        .check(currentLocation.is(baseurl + "/jurisdiction/last-habitual-residence")))
         .pause(continuePause)
 
-    val lastHabitualResidence = exec(http("/njurisdiction/last-habitual-residence")
-        .post("/njurisdiction/last-habitual-residence")
+    val lastHabitualResidence = exec(http("/jurisdiction/last-habitual-residence")
+        .post("/jurisdiction/last-habitual-residence")
         .formParam("jurisdictionLastHabitualResident", "Yes")
         .check(status.is(200))
-        .check(currentLocation.is(baseurl + "/njurisdiction/interstitial")))
+        .check(currentLocation.is(baseurl + "/jurisdiction/interstitial")))
         .pause(continuePause)
 
-    val interstitial = exec(http("/njurisdiction/interstitial")
-        .post("/njurisdiction/interstitial")
+    val interstitial = exec(http("/jurisdiction/interstitial")
+        .post("/jurisdiction/interstitial")
         .formParam("jurisdictionConfidentLegal", "Yes")
         .check(status.is(200))
         .check(currentLocation.is(baseurl + "/petitioner-respondent/confidential")))
