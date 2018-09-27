597eb3f188d5993eb1cb4e7a9715aeeabb47f5fc
tony.chow <tony.chow@hmcts.net>
2018-09-27 06:53:25 +0100

    Used wanted directory naming

---
+++ src/gatling/scenarios/Feeders.scala
@@ -0,0 +1,49 @@
+package scenarios
+
+import com.typesafe.config._
+
+object Feeders {
+
+  val conf = ConfigFactory.load()
+
+  val random = new scala.util.Random
+
+  def randomString(alphabet: String)(n: Int): String =
+    Stream.continually(random.nextInt(alphabet.size)).map(alphabet).take(n).mkString
+
+  def randomAlphanumericString(n: Int) =
+    randomString("abcdefghijklmnopqrstuvwxyz0123456789")(n)
+
+  var generatedEmail = ""
+  var generatedPassword = ""
+
+  def generateEmailAddress() :String = {
+    generatedEmail = ("simulate-delivered-demo-" + randomAlphanumericString(8) + "@notifications.service.gov.uk")
+    generatedEmail
+  }
+
+  def generatePassword() :String = {
+    generatedPassword = randomAlphanumericString(9)
+    generatedPassword
+  }
+
+  private def addIdamUsers(): String = {
+
+    generateEmailAddress()
+    generatePassword()
+
+    var body =
+      s"""{"email":"${generatedEmail}",
+         |"forename":"john",
+         |"surname":"smith",
+         |"userGroup": {"code": "divorce-private-beta"},
+         |"password":"${generatedPassword}"}""".stripMargin
+
+    return body
+  }
+
+  val createIdamUsersFeeder = Iterator.continually(Map("addUser" -> ({
+    addIdamUsers()
+  }), "generatedEmail" -> (generatedEmail), "generatedPassword" -> (generatedPassword)));
+
+}
